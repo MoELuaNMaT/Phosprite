d114f80e4fedc831e24223e4b1dc4bfc5cf6761b
MoELuaNMaT <luanmaaaaaa@gmail.com>
2026-09-19 19:00:34 +0800

    style: format document checker size binding

--- src/UI/Nodes/TransparentChecker.gd
+++ src/UI/Nodes/TransparentChecker.gd
@@ -35,8 +35,7 @@ func update_rect() -> void:
 			for canvas_preview in get_tree().get_nodes_in_group("CanvasPreviews"):
 				canvas_preview.get_viewport().get_node("TransparentChecker").update_rect()
 	material.set_shader_parameter(
-		&"size",
-		document_checker_size if document_pixel_mode else Global.checker_size
+		&"size", document_checker_size if document_pixel_mode else Global.checker_size
 	)
 	material.set_shader_parameter(&"color1", Global.checker_color_1)
 	material.set_shader_parameter(&"color2", Global.checker_color_2)
