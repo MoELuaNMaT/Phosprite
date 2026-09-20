d21ee14d997286cb830a1b77a2ec11ae925a31a8
MoELuaNMaT <luanmaaaaaa@gmail.com>
2026-09-21 00:39:06 +0800

    style: 修正 P3-A UUID helper gdformat

--- src/ProjectLibrary/ProjectIdentity.gd
+++ src/ProjectLibrary/ProjectIdentity.gd
@@ -11,13 +11,16 @@ static func generate_uuid() -> String:
 	bytes[6] = (bytes[6] & 0x0F) | 0x40
 	bytes[8] = (bytes[8] & 0x3F) | 0x80
 	var hex := bytes.hex_encode()
-	return "%s-%s-%s-%s-%s" % [
-		hex.substr(0, 8),
-		hex.substr(8, 4),
-		hex.substr(12, 4),
-		hex.substr(16, 4),
-		hex.substr(20, 12),
-	]
+	return (
+		"%s-%s-%s-%s-%s"
+		% [
+			hex.substr(0, 8),
+			hex.substr(8, 4),
+			hex.substr(12, 4),
+			hex.substr(16, 4),
+			hex.substr(20, 12),
+		]
+	)
 
 
 static func is_valid_uuid(value: String) -> bool:
