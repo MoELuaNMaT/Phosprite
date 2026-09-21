eb7ebe9b7f2963e3a66881a48a8fb6e51e83d4d6
MoELuaNMaT <luanmaaaaaa@gmail.com>
2026-09-21 15:32:11 +0800

    fix: 明确 P3-F 导入来源弹层取消语义

--- src/UI/ProjectGallery/ImportSourceDialog.gd
+++ src/UI/ProjectGallery/ImportSourceDialog.gd
@@ -3,17 +3,32 @@ extends PopupPanel
 
 signal files_requested
 signal photos_requested
+signal dismissed
+
+var _choice_made := false
+
+
+func _ready() -> void:
+	popup_hide.connect(_on_popup_hide)
 
 
 func popup_for_import() -> void:
+	_choice_made = false
 	popup_centered()
 
 
 func _on_files_pressed() -> void:
-	hide()
+	_choice_made = true
 	files_requested.emit()
+	hide()
 
 
 func _on_photos_pressed() -> void:
-	hide()
+	_choice_made = true
 	photos_requested.emit()
+	hide()
+
+
+func _on_popup_hide() -> void:
+	if not _choice_made:
+		dismissed.emit()
