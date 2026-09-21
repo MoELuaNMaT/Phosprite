411dc5948db13ad31eefe5049181bdd494983933
MoELuaNMaT <luanmaaaaaa@gmail.com>
2026-09-21 15:32:15 +0800

    fix: 明确 P3-F 图片模式取消语义

--- src/UI/ProjectGallery/ImageImportModeDialog.gd
+++ src/UI/ProjectGallery/ImageImportModeDialog.gd
@@ -3,17 +3,32 @@ extends PopupPanel
 
 signal layer_requested
 signal reference_requested
+signal dismissed
+
+var _choice_made := false
+
+
+func _ready() -> void:
+	popup_hide.connect(_on_popup_hide)
 
 
 func popup_for_image() -> void:
+	_choice_made = false
 	popup_centered()
 
 
 func _on_layer_pressed() -> void:
-	hide()
+	_choice_made = true
 	layer_requested.emit()
+	hide()
 
 
 func _on_reference_pressed() -> void:
-	hide()
+	_choice_made = true
 	reference_requested.emit()
+	hide()
+
+
+func _on_popup_hide() -> void:
+	if not _choice_made:
+		dismissed.emit()
