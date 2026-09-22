d48e47e8aff43a385f331b1ba6ed0a778087b30e
MoELuaNMaT <luanmaaaaaa@gmail.com>
2026-09-23 00:58:32 +0800

    refactor(palette): use native touch split dragger

--- src/UI/Workspace/PaletteColorPanel.gd
+++ src/UI/Workspace/PaletteColorPanel.gd
@@ -1,16 +1,12 @@
 class_name PaletteColorPanel
 extends VSplitContainer
 
-const TOUCH_SPLITTER_SLOP := 14.0
 const COMPACT_PALETTE_BOTTOM_PADDING := 4.0
 
 var _color_options_expanded := false
 var _normal_palette_height := -1.0
 var _expanded_manual_override := false
 var _applying_auto_split := false
-var _touch_drag_index := -1
-var _touch_drag_origin_y := 0.0
-var _touch_drag_start_offset := 0
 
 @onready var palettes := $Palettes as PalettePanel
 @onready var color_picker_panel := $"Color Picker"
@@ -18,6 +14,7 @@ var _touch_drag_start_offset := 0
 
 func _ready() -> void:
 	dragged.connect(_on_split_dragged)
+	drag_ended.connect(_on_split_drag_ended)
 	resized.connect(_on_panel_resized)
 	if color_picker_panel.has_signal(&"color_options_toggled"):
 		color_picker_panel.connect(&"color_options_toggled", _on_color_options_toggled)
@@ -36,15 +33,6 @@ func _ready() -> void:
 func _exit_tree() -> void:
 	if Palettes.palette_selected.is_connected(_on_palette_selected):
 		Palettes.palette_selected.disconnect(_on_palette_selected)
-
-
-func _input(event: InputEvent) -> void:
-	if not is_visible_in_tree():
-		return
-	if event is InputEventScreenTouch:
-		_handle_split_touch(event as InputEventScreenTouch)
-	elif event is InputEventScreenDrag:
-		_handle_split_drag(event as InputEventScreenDrag)
 
 
 func _capture_normal_palette_height() -> void:
@@ -81,7 +69,10 @@ func _on_split_dragged(_offset: int) -> void:
 		return
 	if _color_options_expanded:
 		_expanded_manual_override = true
-	else:
+
+
+func _on_split_drag_ended() -> void:
+	if not _color_options_expanded:
 		_normal_palette_height = palettes.size.y
 
 
@@ -102,45 +93,15 @@ func _set_palette_height(target_height: float) -> void:
 	var delta := target_height - palettes.size.y
 	if absf(delta) < 0.5:
 		return
+	var offsets := split_offsets
+	if offsets.is_empty():
+		offsets = PackedInt32Array([0])
+	offsets[0] += roundi(delta)
 	_applying_auto_split = true
-	split_offset += roundi(delta)
+	split_offsets = offsets
+	clamp_split_offset()
 	_applying_auto_split = false
-
-
-func _handle_split_touch(event: InputEventScreenTouch) -> void:
-	if event.pressed:
-		if _touch_drag_index != -1 or not _splitter_hit_test(event.position):
-			return
-		_touch_drag_index = event.index
-		_touch_drag_origin_y = event.position.y
-		_touch_drag_start_offset = split_offset
-		get_viewport().set_input_as_handled()
-		return
-	if event.index != _touch_drag_index:
-		return
-	_touch_drag_index = -1
-	get_viewport().set_input_as_handled()
-
-
-func _handle_split_drag(event: InputEventScreenDrag) -> void:
-	if event.index != _touch_drag_index:
-		return
-	var delta_y := event.position.y - _touch_drag_origin_y
-	split_offset = _touch_drag_start_offset + roundi(delta_y)
 	queue_redraw()
-	if _color_options_expanded:
-		_expanded_manual_override = true
-	else:
-		_normal_palette_height = palettes.size.y
-	get_viewport().set_input_as_handled()
-
-
-func _splitter_hit_test(viewport_position: Vector2) -> bool:
-	var rect := get_global_rect()
-	if viewport_position.x < rect.position.x or viewport_position.x > rect.end.x:
-		return false
-	var splitter_y := palettes.get_global_rect().end.y
-	return absf(viewport_position.y - splitter_y) <= TOUCH_SPLITTER_SLOP
 
 
 func _draw() -> void:
